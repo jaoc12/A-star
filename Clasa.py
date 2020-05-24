53c2447e12c74ccf8bd502cef7056a91405dda4e
jaoc12 <chitu.stefan@yahoo.com>
2020-05-24 17:36:55 +0300

    Adaugat comentarii

--- Clasa.py
+++ Clasa.py
@@ -1,15 +1,14 @@
-
-""" definirea problemei """
+# 242 Chitu Stefan Catalin
 import time
 
 
 class Nod:
     def __init__(self, info, suparati, clasa, semn_mutare, h=0):
-        self.info = info
+        self.info = info                    # in info memoram la ce elev se afla biletul
         self.h = h
-        self.suparati = suparati
-        self.clasa = clasa
-        self.semn_mutare = semn_mutare
+        self.suparati = suparati            # lista elevilor suparati unul pe celalalt
+        self.clasa = clasa                  # matrice cu clasa
+        self.semn_mutare = semn_mutare      # directia in care a fost dat biletul
 
     def __str__(self):
         return "({}, h={})".format(self.info, self.h)
@@ -23,24 +22,14 @@ class Problema:
         self.nod_start, self.nod_scop = read_input(filename)
 
 
-""" Sfarsit definire problema """
-
-""" Clase folosite in algoritmul A* """
-
-
 class NodParcurgere:
-    """O clasa care cuprinde informatiile asociate unui nod din listele open/closed
-        Cuprinde o referinta catre nodul in sine (din graf)
-        dar are ca proprietati si valorile specifice algoritmului A* (f si g).
-        Se presupune ca h este proprietate a nodului din graf
-    """
 
-    problema = None		# atribut al clasei (se suprascrie jos in __main__)
+    problema = None
 
     def __init__(self, nod_graf, parinte=None, f=None):
-        self.nod_graf = nod_graf    # obiect de tip Nod
-        self.parinte = parinte		# obiect de tip NodParcurgere
-        self.g = 1					# costul drumului de la radacina pana la nodul curent
+        self.nod_graf = nod_graf
+        self.parinte = parinte
+        self.g = 1
         if f is None :
             self.f = self.g + self.nod_graf.h
         else:
@@ -51,29 +40,31 @@ class NodParcurgere:
         count = 0
 
         if euristica == 0:
+            # folosim pentru h distanta manhattan de la elevul curent pana la cel scop
+            # o sa subestimeze distanta deoarece nu ia in considerare restrictiile de deplasare
             x_nod, y_nod = self.gaseste_elev(info)
             x_scop, y_scop = self.gaseste_elev(solutie)
             count = abs(x_nod - x_scop) + abs(y_nod - y_scop)
 
         if euristica == 1:
+            # h reprezinta cate randuri de banci se afla intre elevul curent si cel scop
+            # o sa subestimeze distanta deoarece nu stie daca elevul scop este imediat pe coloana urmatoare
+            # sau in capatul ei
             x_nod, y_nod = self.gaseste_elev(info)
             x_scop, y_scop = self.gaseste_elev(solutie)
             count = abs(y_nod/2 - y_scop/2)
 
         if euristica == 3:
+            # prefera sa ramana pe aceeasi linie
+            # poate supraestima daca elevul scop este in fata sau in spatele celui actual
             x_nod, y_nod = self.gaseste_elev(info)
-            x_scop, y_scop = self.gaseste_elev(solutie)
             x_tata, y_tata = self.gaseste_elev(self.nod_graf.info)
-            count = abs(x_nod - x_scop) + abs(y_nod - y_scop)
-            count /= (abs(x_tata - x_nod) + 1)
+            count = abs(x_tata - x_nod)
 
         return count
 
     def drum_arbore(self):
-        """
-            Functie care calculeaza drumul asociat unui nod din arborele de cautare.
-            Functia merge din parinte in parinte pana ajunge la radacina
-        """
+        # reface drumul de la nodul curent la radacina
         nod_c = self
         drum = [nod_c]
         while nod_c.parinte is not None:
@@ -82,16 +73,7 @@ class NodParcurgere:
         return drum
 
     def contine_in_drum(self, nod):
-        """
-            Functie care verifica daca nodul "nod" se afla in drumul dintre radacina si nodul curent (self).
-            Verificarea se face mergand din parinte in parinte pana la radacina
-            Se compara doar informatiile nodurilor (proprietatea info)
-            Returnati True sau False.
-
-            "nod" este obiect de tip Nod (are atributul "nod.info")
-            "self" este obiect de tip NodParcurgere (are "self.nod_graf.info")
-        """
-
+        # verifica daca se afla in drum deja
         drum = self.drum_arbore()
         for nod_c in drum:
             if nod_c.nod_graf.info == nod.info:
@@ -99,12 +81,15 @@ class NodParcurgere:
         return False
 
     def gaseste_elev(self, info):
+        # cauta coordonatele elevului dat ca argument
         for i in range(len(self.nod_graf.clasa)):
             for j in range(6):
                 if self.nod_graf.clasa[i][j] == info:
                     return i, j
 
     def suparati(self, elev_1, elev_2):
+        # verifica daca doi elevi dati sunt in lista celor suparati
+        # relatia de suparare este reciproca
         for suparati in self.nod_graf.suparati:
             if suparati[0] == elev_1:
                 if suparati[1] == elev_2:
@@ -114,36 +99,31 @@ class NodParcurgere:
                     return True
         return False
 
-    # se modifica in functie de problema
     def expandeaza(self):
-        """Pentru nodul curent (self) parinte, trebuie sa gasiti toti succesorii (fiii)
-        si sa returnati o lista de tupluri (nod_fiu, cost_muchie_tata_fiu),
-        sau lista vida, daca nu exista niciunul.
-        (Fiecare tuplu contine un obiect de tip Nod si un numar.)
-        """
-
         succesori = []
         info = self.nod_graf.info
         clasa = self.nod_graf.clasa
-        vecini = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+        vecini = [[0, -1], [0, 1], [-1, 0], [1, 0]]     # lista cu cei 4 vecini posibili
         x, y = self.gaseste_elev(info)
         for i, coordonate in enumerate(vecini):
             x_vecin = x + coordonate[0]
             y_vecin = y + coordonate[1]
             if 0 > x_vecin or x_vecin >= len(clasa) or 0 > y_vecin or y_vecin >= 6:
-                continue
+                continue    # daca pozitia noua nu este in matrice trecem la urmatoarea
             vecin = clasa[x_vecin][y_vecin]
             ultima_banca = False
             if self.suparati(info, vecin) is True or vecin == 'liber':
-                continue
+                continue    # daca elevii sunt ceratati trecem la urmatoarea posibilitate
             if x in list(range(len(clasa)))[-2:]:
-                ultima_banca = True
+                ultima_banca = True     # testam daca se afla in ultimele doua randuri
             if i == 0:
+                # daca da biletul in stanga ne asiguram ca se afla in spate sau e pe pozitia dreapta din rand
                 if y % 2 == 0 and ultima_banca is True:
                     succesori.append((Nod(vecin, self.nod_graf.suparati, clasa, "<<", self.funct_h(vecin)), 1))
                 if y % 2 == 1:
                     succesori.append((Nod(vecin, self.nod_graf.suparati, clasa, "<", self.funct_h(vecin)), 1))
             if i == 1:
+                # analog cu stanga
                 if y % 2 == 1 and ultima_banca is True:
                     succesori.append((Nod(vecin, self.nod_graf.suparati, clasa, ">>", self.funct_h(vecin)), 1))
                 if y % 2 == 0:
@@ -155,16 +135,13 @@ class NodParcurgere:
 
         return succesori
 
-    # se modifica in functie de problema
     def test_scop(self):
+        # vedem daca am ajuns la elevul vizat
         return self.nod_graf.info == self.problema.nod_scop
 
     def __str__(self):
         parinte = self.parinte if self.parinte is None else self.parinte.nod_graf.info
         return f"({self.nod_graf}, parinte={parinte}, f={self.f}, g={self.g})"
-
-
-""" Algoritmul A* """
 
 
 def read_input(file_name):
@@ -174,6 +151,10 @@ def read_input(file_name):
     clasa = []
     suparati = []
     info = ''
+    # avem 3 etape de citit
+    # pozitia in clasa - flag = 1
+    # lista de elevi suparati - flag = 2
+    # de la cine pentru cine este mesajul - flag = 3
     for i in range(len(temp)):
         if temp[i] == 'suparati':
             flag = 2
@@ -189,10 +170,6 @@ def read_input(file_name):
 
 
 def in_lista(lista, nod):
-    """
-    lista "l" contine obiecte de tip NodParcurgere
-    "nod" este de tip Nod
-    """
     for i in range(len(lista)):
         if lista[i].nod_graf.info == nod.info:
             return lista[i]
@@ -200,6 +177,7 @@ def in_lista(lista, nod):
 
 
 def afisare_elev(nod):
+    # afiseaza un elev plus directia din care a venit mesajul la el
     sir = ""
     if nod.nod_graf.semn_mutare != "":
         sir += nod.nod_graf.semn_mutare + " "
@@ -216,8 +194,8 @@ def afisare_simpla(lista):
 
 def a_star():
     rad_arbore = NodParcurgere(NodParcurgere.problema.nod_start)
-    open = [rad_arbore]		# open va contine elemente de tip NodParcurgere
-    closed = []				# closed va contine elemente de tip NodParcurgere
+    open = [rad_arbore]
+    closed = []
     count = 0
     while len(open) > 0:
         nod_curent = open[0]
@@ -230,25 +208,25 @@ def a_star():
         for s in succersori:
             flag = False
             nod_nou = NodParcurgere(s[0], nod_curent)
-            if nod_curent.contine_in_drum(s[0]) is False:
+            if nod_curent.contine_in_drum(s[0]) is False:   # verificam daca nodul e deja in drum
                 nod_open = in_lista(open, s[0])
-                if nod_open is not None:
+                if nod_open is not None:    # daca este in open testam daca f curent este mai bun
                     if nod_open.f > nod_nou.f:
                         open.remove(nod_open)
                         flag = True
                 nod_closed = in_lista(closed, s[0])
-                if nod_closed is not None:
+                if nod_closed is not None:  # daca este in closed testam daca f curent este mai bun
                     if nod_closed.f > nod_nou.f:
                         closed.remove(nod_closed)
                         flag = True
                 if nod_open is None and nod_closed is None:
                     flag = True
                 if flag is True:
-                    if len(open) == 0:
+                    if len(open) == 0:  # daca nu este in nicio lista si open e gol il punem direct
                         open.append(nod_nou)
                     else:
                         adaugat = False
-                        for i in range(len(open)):
+                        for i in range(len(open)):  # cautam locul potrivit in open
                             if open[i].f > nod_nou.f:
                                 open = open[:i] + [nod_nou] + open[i:]
                                 adaugat = True
@@ -260,6 +238,7 @@ def a_star():
                         if adaugat is False:
                             open.append(nod_nou)
 
+    # producem mesajul de output
     mesaj = "\n------------------ Concluzie -----------------------\n"
     if len(open) == 0:
         return mesaj + "Lista open e vida, nu avem drum de la nodul start la nodul scop", count
@@ -273,7 +252,7 @@ if __name__ == "__main__":
         problema = Problema(fisier_intrare)
         NodParcurgere.problema = problema
         global euristica
-        for i in range(3):
+        for i in range(3):  # testam inputul pentru fiecare euristica
             euristica = i
             if i == 0:
                 f = open(f"output_{nr + 1}.txt", "wt")
@@ -282,7 +261,7 @@ if __name__ == "__main__":
             t_inainte = int(round(time.time() * 1000))
             mesaj, count = a_star()
             t_dupa = int(round(time.time() * 1000))
-            mesaj += f"\nTimpul necesar a fost {t_dupa - t_inainte} milisecunde.\n"
+            mesaj += f"\nTimpul necesar a fost de {t_dupa - t_inainte} milisecunde.\n"
             mesaj += f"A fost nevoie de {count} de mutari.\n"
             f.write(mesaj)
             f.close()
